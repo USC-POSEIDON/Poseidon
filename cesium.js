99237c7b5090375590718c2c431ef90d72aa8d69
Changzhong Qian <sidqian@usc.edu>
2024-01-22 08:41:38 -0800

    added cal

---
+++ cesium.js
@@ -0,0 +1,15 @@
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    animation: false,      
+    baseLayerPicker: false, 
+    fullscreenButton: false, 
+    vrButton: false,        
+    geocoder: false,        
+    homeButton: false,      
+    infoBox: false,         
+    sceneModePicker: false, 
+    selectionIndicator: false, 
+    timeline: false,        
+    navigationHelpButton: false, 
+    navigationInstructionsInitiallyVisible: false,
+    creditContainer: document.createElement('div') 
+  });
