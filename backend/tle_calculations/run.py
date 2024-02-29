87caadf800c2a6bf2e7436a00958b26b8da46958
sidqian <sidqian@usc.edu>
2024-02-29 15:56:56 -0800

    Revert "Merge pull request #2 from CSCI401TEAM/POS-127-TLE-Testing"
    This reverts commit 0678e4053481d4febf9f14826d7f95d04aeb7ddf, reversing
    changes made to 6be87c1d906c622933458ffc83dbb736a06d1199.

--- backend/tle_calculations/run.py
+++
@@ -1,8 +0,0 @@
-from tle_calculations import app
-from tle_calculations.celestrak_calls import getLogin, createTables
-
-if __name__ == '__main__':
-    print("flask startup :)")
-    # getLogin()
-    createTables()
-    app.run()
--- backend/tle_calculations/unitTests/tle_calculation_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from unittest.mock import patch, MagicMock
-from flask import Flask
-from tle_calculations.orbit_calculations import getPassTimeInfo, app
-
-"""
-Test suite for the TLE calculations
-"""
